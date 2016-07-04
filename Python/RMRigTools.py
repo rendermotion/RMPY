fb1ba2d2a7c02fcfbeaf0b24e2c04d74b5d5c61c
Daniel Ruiz Vargas <daniel@rendermotion.com>
2016-07-03 20:00:08 -0500

    Correcciones para evitar los errores en commit

--- Python/RMRigTools.py
+++ Python/RMRigTools.py
@@ -28,62 +28,6 @@ def connectWithLimits(AttrX,AttrY,keys):
     for eachKey  in keys:
         cmds.setDrivenKeyframe(AttrY, currentDriver = AttrX,dv = eachKey[0],v =eachKey[1])
 
-def RMLockAndHideAttributes(ObjArray,BitString):
-    InfoDic = {".translateX":0,
-                ".translateY":1,
-                ".translateZ":2,
-                ".rotateX":3,
-                ".rotateY":4,
-                ".rotateZ":5,
-                ".scaleX":6,
-                ".scaleY":7,
-                ".scaleZ":8,
-                ".visibility":9}
-    if (len(BitString)==10):
-        for eachObj in ObjArray:
-            for parameter in InfoDic:
-                if BitString[InfoDic[parameter]]==0:
-                    cmds.setAttr(eachObj+parameter,k=False,l=True)
-                else:
-                    cmds.setAttr(eachObj+parameter,k=True,l=False)
-
-def RMLinkHerarchyRotation(jntStart, jntEnd, Ctrl,X=True ,Y=True ,Z=True):
-    children = cmds.listRelatives(jntStart,children=True)
-    if(jntStart == jntEnd):
-        return True
-    else:
-        for joints in children:
-            if RMIsInHierarchy(joints,jntEnd):
-                if X:
-                    cmds.connectAttr( Ctrl+".rotateX", jntStart+".rotateX")
-                if Y:
-                    cmds.connectAttr( Ctrl+".rotateY", jntStart+".rotateY")
-                if Z:
-                    cmds.connectAttr( Ctrl+".rotateZ", jntStart+".rotateZ")
-                return RMLinkHerarchyRotation(joints,jntEnd,Ctrl,X,Y,Z)
-            else:
-                return False
-
-def RMIsInHierarchy(Obj1,Obj2):
-    children=cmds.listRelatives(Obj1,children=True)
-    if Obj1 == Obj02:
-        return True
-    else:
-        for eachChild in children:
-            if RMIsInHierarchy(eachChild,Obj2):
-                return True
-    return False
-
-def RMCreateBonesAtPoints(PointArray):
-    jointArray = []
-    
-
-
-    Obj1Position = cmds.xform(PointArray[0], rp=True, ws=True, q=True)
-    Obj2Position = cmds.xform(PointArray[1], rp=True, ws=True, q=True)
-    #for values in Obj1Position:
-    #    values-Obj2Position[]
-
 def RMCreateGroupOnObj(Obj,Type="world"):
     '''
     "world","child","parent","inserted"
@@ -134,7 +78,6 @@ def RMParentArray (Parent, Array):
     for objects in Array:
         cmds.parent(objects,Parent)
 
-
 def RMCreateLineBetwenPoints (Point1, Point2):
 
     Curve = cmds.curve (degree=1, p=[[0,0,0],[1,0,0]])
@@ -148,3 +91,57 @@ def RMCreateLineBetwenPoints (Point1, Point2):
     print Cluster1Handle
     print (str(cmds.objectType(Cluster1)))
     print (str(cmds.objectType(Cluster1Handle)))
+
+def RMLockAndHideAttributes(ObjArray,BitString):
+    InfoDic = {".translateX":0,
+                ".translateY":1,
+                ".translateZ":2,
+                ".rotateX":3,
+                ".rotateY":4,
+                ".rotateZ":5,
+                ".scaleX":6,
+                ".scaleY":7,
+                ".scaleZ":8,
+                ".visibility":9}
+    if (len(BitString)==10):
+        for eachObj in ObjArray:
+            for parameter in InfoDic:
+                if BitString[InfoDic[parameter]]==0:
+                    cmds.setAttr(eachObj+parameter,k=False,l=True)
+                else:
+                    cmds.setAttr(eachObj+parameter,k=True,l=False)
+
+def RMLinkHerarchyRotation(jntStart, jntEnd, Ctrl,X=True ,Y=True ,Z=True):
+    children = cmds.listRelatives(jntStart,children=True)
+    if(jntStart == jntEnd):
+        return True
+    else:
+        for joints in children:
+            if RMIsInHierarchy(joints,jntEnd):
+                if X:
+                    cmds.connectAttr( Ctrl+".rotateX", jntStart+".rotateX")
+                if Y:
+                    cmds.connectAttr( Ctrl+".rotateY", jntStart+".rotateY")
+                if Z:
+                    cmds.connectAttr( Ctrl+".rotateZ", jntStart+".rotateZ")
+                return RMLinkHerarchyRotation(joints,jntEnd,Ctrl,X,Y,Z)
+            else:
+                return False
+
+def RMIsInHierarchy(Obj1,Obj2):
+    children=cmds.listRelatives(Obj1,children=True)
+    if Obj1 == Obj02:
+        return True
+    else:
+        for eachChild in children:
+            if RMIsInHierarchy(eachChild,Obj2):
+                return True
+    return False
+
+def RMCreateBonesAtPoints(PointArray):
+    jointArray = []
+    Obj1Position = cmds.xform(PointArray[0], rp=True, ws=True, q=True)
+    Obj2Position = cmds.xform(PointArray[1], rp=True, ws=True, q=True)
+    #for values in Obj1Position:
+    #    values-Obj2Position[]
+
