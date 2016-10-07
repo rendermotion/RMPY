43dee45d9f8f0173199307133a17a6f5cd8cdbb0
Daniel Ruiz Vargas <daniel@rendermotion.com>
2016-10-07 08:26:35 -0500

    More Changes to Facial Rig
    MOre Facial RIg

--- Python/snippets/simpleRename.py
+++ Python/snippets/simpleRename.py
@@ -2,4 +2,4 @@ import RMNameConvention
 NameConv  = RMNameConvention.RMNameConvention()
 selection = cmds.ls(selection=True)
 for i in selection:
-	 NameConv.RMRenameNameInFormat(i, Side = "RH",System = "intRig")
+	 NameConv.RMRenameNameInFormat(i,Side = "MD",Type='control',System = "rig")
