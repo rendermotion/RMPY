ee9a75b01aaa5fa2fc68800c6e65b3bfcdab78e9
Daniel Ruiz Vargas <daniel@rendermotion.com>
2016-12-13 07:21:16 -0600

    2 simples commits
    2 cambios en las funciones de renombrar

--- Python/snippets/simpleRename.py
+++ Python/snippets/simpleRename.py
@@ -3,6 +3,7 @@ NameConv  = RMNameConvention.RMNameConvention()
 reload (RMNameConvention)
 selection = cmds.ls(selection=True)
 for i in selection:
-	NameConv.RMRenameNameInFormat(i, Side = "RH", System = "TorsoBelts")
+	#NameConv.RMRenameNameInFormat(i, Side = "RH", System = "TorsoBelts")
+	cmds.rename(i, "%s%s"%("ButterflyGirl_",i))
 	#NameConv.RMRenameSetFromName( i , "RH","Side")
 
