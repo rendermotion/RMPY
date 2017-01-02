f4713a04797dc37189ef1839b27f238e726fe478
Daniel Ruiz <Daniel Ruiz>
2017-01-02 11:10:52 -0600

    e agrego un nuevo script para generar un control en joints seleccionados

--- Python/snippets/simpleRename.py
+++ Python/snippets/simpleRename.py
@@ -3,10 +3,10 @@ NameConv  = RMNameConvention.RMNameConvention()
 reload (RMNameConvention)
 selection = cmds.ls(selection=True)
 for i in selection:
-	#NameConv.RMRenameNameInFormat(i, Side = "RH", System = "TorsoBelts")
+	#NameConv.RMRenameNameInFormat(i, Side = "MD", System = "rig")
 	#cmds.rename(i, "%s%s"%("ButterflyGirl_",i))
 	#NameConv.RMRenameSetFromName( i , "RH","Side")
-	newName = NameConv.RMRenameSetFromName( i , "LF","Side")
-	NameConv.RMRenameSetFromName( newName , "LashesOut","Name")
+	#newName = NameConv.RMRenameSetFromName( i , "MD","Side")
+	NameConv.RMRenameSetFromName( i , "DelantalRivet","Name")
 
 
