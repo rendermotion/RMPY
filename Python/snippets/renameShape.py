21aea0bd7afcb2985cc94c5b44562108b01655bb
Daniel Ruiz Vargas <daniel@rendermotion.com>
2017-01-02 19:29:35 -0600

    algunos snippets
    se crearonb snippets para crear joints

---
+++ Python/snippets/renameShape.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+
+
+
+def RenameHierarchyShapes(SceneObject):
+	shapes =  cmds.listRelatives(SceneObject,path = True, children = True, shapes=True )
+	print shapes
+	if shapes:
+		cmds.rename(shapes,"%sShape"%SceneObject)
+	childList = cmds.listRelatives(SceneObject, children = True)
+	if childList:
+		for eachChild in childList:
+			RenameHierarchyShapes(eachChild)
+
+if __name__=="__main__":
+	RenameHierarchyShapes("Face")
