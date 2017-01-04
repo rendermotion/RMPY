812ee010c91ec3da29e7dd65d751973908f9b050
Daniel Ruiz <danielkenobi3d@gmail.com>
2017-01-04 13:52:29 -0600

    se creo un script para realizar el doblez de papeles
    se creo paperfold, y se creo una nueva clase para manejar los parametros
    de usuario en los objetos

---
+++ Python/RMUserParameters.py
@@ -0,0 +1,14 @@
+import maya.cmds as cmds
+
+class RMUserParameters(object):
+    def __init__ (self, objectBaseName):
+        self.object = objectBaseName
+
+    def addNumeric (self, Name, limits = [0,10]):
+        AttributeList = cmds.listAttr(self.object)
+        if Name  in AttributeList:
+            print "the Object Allready has an Attribute with this name, the type is:",cmds.getAttr (self.object + "." + Name,type = True)
+            return False
+        else :
+            cmds.addAttr(self.object,at = "float", ln = Name,  hnv = 1, hxv = 1, h = 0, k = 1, smn = limits[0], smx = limits[1])
+            return True
