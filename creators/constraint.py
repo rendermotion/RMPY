bb96f46ffe256f5fc7b62298832cdca4cea57ad1
Daniel Ruiz Vargas <daniel@rendermotion.com>
2019-07-06 09:54:06 -0400

    constraint
    the first pass of functions of the constraint creator class had been done

--- creators/constraint.py
+++ creators/constraint.py
@@ -5,35 +5,42 @@ from RMPY.creators import creatorsBase
 
 class Constraint(creatorsBase.CreatorBase):
     def __init__(self, *args, **kwargs):
-        super(Constraint, self).__init__(**kwargs)
+        super(Constraint, self).__init__(*args, **kwargs)
+        self.constraint_type = []
+        self.define_constraints(**kwargs)
 
     def node_base(self, *args, **kwargs):
+        if len(args) >= 2:
+                self.constraint(*args, constraints_list=self.constraint_type, **kwargs)
+        else:
+            raise AttributeError('you should provide at least 2 attributes')
+
+    def node_list_base(self, *args, **kwargs):
+        if len(args) >= 2 and len(args[0]) == len(args[1]):
+            for driver, driven in zip(args[0], args[1]):
+                self.constraint(driver, driven, constraints_list=self.constraint_type, **kwargs)
+
+    def define_constraints(self, **kwargs):
         parent = kwargs.pop('parent', True)
         point = kwargs.pop('point', False)
         orient = kwargs.pop('orient', False)
         scale = kwargs.pop('scale', True)
-        constraint_type = []
-
         if parent:
-            constraint_type.append(pm.parentConstraint)
+            self.constraint_type.append(pm.parentConstraint)
         if point:
-            constraint_type.append(pm.pointConstraint)
+            self.constraint_type.append(pm.pointConstraint)
         if orient:
-            constraint_type.append(pm.orientConstraint)
+            self.constraint_type.append(pm.orientConstraint)
         if scale:
-            constraint_type.append(pm.scaleConstraint)
+            self.constraint_type.append(pm.scaleConstraint)
 
-        if len(args) >= 2:
-            if parent:
-                for each in args[1:]:
-                    pm.parentConstraint()
-            if len(args) == 2:
-                if args[0].__class__ is not list and args[1].__class__ is not list:
-                    self.constraint(args[0], args[1],
-                                    constraints_list=constraint_type)
-                    
-        else:
-            raise AttributeError('you should provide at least 2 attributes')
-    def constraint(self, *args):
-        for each_constraint in self.constraints:
-        
+    def constraint(self, *args, **kwargs):
+        constraints = []
+        for each_constraint in self.constraint_type:
+            constraints.append(each_constraint(*args, **kwargs))
+
+
+if __name__ == '__main__':
+    selection = pm.ls(selection=True)
+    constraint = Constraint()
+    constraint.node_base(*selection, mo=True)
