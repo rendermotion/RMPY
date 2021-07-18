3e59cbcc0570727e720050458f9682ee762a510e
Daniel Ruiz Vargas <daniel@rendermotion.com>
2021-07-18 15:43:52 -0400

    build biped

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
