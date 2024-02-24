92f513082ea0c0206e9e63b57af0055ca5c7907c
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-02-23 19:16:47 -0500

    Adding a search by token names

--- snippets/selection_filters.py
+++ snippets/selection_filters.py
@@ -23,3 +23,16 @@ result_list = []
 for each in selection:
     result_list.extend(search_hierarchy.none_skinned_mesh_in_hierarchy(each))
 pm.select(result_list)
+
+
+from RMPY.core import search_hierarchy
+import pymel.core as pm
+selection = pm.ls(selection=True)
+result_list = []
+for each in selection:
+    result_list.extend(search_hierarchy.by_token_in_name(each, '_ctl'))
+pm.select(result_list)
+
+
+
+
