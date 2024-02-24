6217205e50c0072326d7fda2231e7a6d61dc2257
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-02-23 19:12:28 -0500

    Added some buttons to selction

---
+++ snippets/selection_filters.py
@@ -0,0 +1,25 @@
+from RMPY.core import search_hierarchy
+import pymel.core as pm
+selection = pm.ls(selection=True)
+result_list = []
+for each in selection:
+    result_list.extend(search_hierarchy.type_in_hierarchy(each))
+pm.select(result_list)
+
+
+from RMPY.core import search_hierarchy
+import pymel.core as pm
+selection = pm.ls(selection=True)
+result_list = []
+for each in selection:
+    result_list.extend(search_hierarchy.skinned_mesh_in_hierarchy(each))
+pm.select(result_list)
+
+
+from RMPY.core import search_hierarchy
+import pymel.core as pm
+selection = pm.ls(selection=True)
+result_list = []
+for each in selection:
+    result_list.extend(search_hierarchy.none_skinned_mesh_in_hierarchy(each))
+pm.select(result_list)
