12c4084f6ccfb4a5427723514e657a1382efc60b
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-03-17 23:43:51 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,6 +68,7 @@ extensions = [
     'sphinx.ext.autosummary',
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
+    'sphinx_rtd_theme'
 ]
 intersphinx_mapping = {
     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
