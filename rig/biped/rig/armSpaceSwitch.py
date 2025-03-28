c74b67b8bec22cfaa6c25ee5b26c3c96d4ccfbaa
Daniel Ruiz Vargas <daniel@rendermotion.com>
2025-03-27 21:50:13 -0400

    Update armSpaceSwitch.py

--- rig/biped/rig/armSpaceSwitch.py
+++ rig/biped/rig/armSpaceSwitch.py
@@ -13,7 +13,7 @@ class ArmSpaceSwitch(rigBase.RigBase):
                                                                   attribute_name='space',
                                                                   control=arm_rig.rig_arm.ik_rig.controls[0],
                                                                   parent=True, scale=True, rig_system=arm_rig.rig_system)
-        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.controls[0])
+        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.reset_controls[0])
         # Creates the ik pole vector control spaceswitch
         enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch(world_rig.tip, arm_rig.rig_clavicle.tip, arm_rig.rig_arm.ik_rig.controls[0],
                                                                   alias_list=['world', 'shoulder', 'ikHandle'],
@@ -21,7 +21,7 @@ class ArmSpaceSwitch(rigBase.RigBase):
                                                                   control=arm_rig.rig_arm.ik_rig.controls[1],
                                                                   parent=True, scale=True,
                                                                   rig_system=arm_rig.rig_system)
-        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.controls[1])
+        enum_space_switch.add_object(arm_rig.rig_arm.ik_rig.reset_controls[1])
         # creates the fk control spaceswitch
         enum_space_switch = rigEnumSpaceSwitch.RigEnumSpaceSwitch(arm_rig.rig_clavicle.tip, world_rig.tip,
                                                                   cog_rig.tip,
